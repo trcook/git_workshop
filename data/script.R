9e0ba11b33e7a6d4d52a728ecaa2ac9c225d2370
Thomas Cook <trcook@gmail.com>
2015-10-23 09:16:00 -0600

    simplified code

--- data/script.R
+++ data/script.R
@@ -10,23 +10,8 @@ z<-rnorm(100,x+y,1)
 ### Some other stuff
 
 dat<-read.csv("./pristine/state_data_2.csv")
-
-summary(dat)
-ls(dat)
-apply(dat,2,summary)
+#' need to remove washington dc
+dat<-dat[-which(dat$State=="District of Columbia"),]
+dat
 summary(glm(as.factor(party)~fed_need+openness,family=binomial,data=dat))
-#'' eh? this data is weird
-
-
-#' Upon further inspection, there are too many states:
-length(dat[,1])
-
-#' checking for duplicates
-length(which(duplicated(dat$State)))
-#' hmm. no duplicates
-
-levels(dat$State)
-# Oh, washington isn't a state
-len(setdiff(dat$State,"District of Columbia"))
-
-dat<-dat[-which(dat$State=="District of Columbia"),1]
+# Looks right to me
