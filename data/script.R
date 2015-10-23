7d71aeb02331a1197d98ea433981cbac406c69d7
Thomas Cook <trcook@gmail.com>
2015-10-23 09:11:36 -0600

    identified problem. data includes washington. fixing

--- data/script.R
+++ data/script.R
@@ -1,5 +1,8 @@
 require(foreign)
 
+# convenience function to print out length of object
+len=function(x){print(length(x))}
+
 x<-runif(100,0,1)
 y<-rnorm(100,x,1)
 z<-rnorm(100,x+y,1)
@@ -17,3 +20,11 @@ summary(glm(as.factor(party)~fed_need+openness,family=binomial,data=dat))
 
 #' Upon further inspection, there are too many states:
 length(dat[,1])
+
+#' checking for duplicates
+length(which(duplicated(dat$State)))
+#' hmm. no duplicates
+
+levels(dat$State)
+# Oh, washington isn't a state
+len(setdiff(dat$State,"District of Columbia"))
